bf71991a565bd07bb1a4d86928f0d8936bc1488a
Nguyen Duc Duy <duydeptrai1611@gmail.com>
2025-09-27 22:05:32 -0500

    update Link_finder.py

---
+++ link_finder.py
@@ -0,0 +1,24 @@
+from html.parser import HTMLParser
+from urllib import parse
+
+class LinkFinder(HTMLParser):
+
+    def __init__(self):
+        super().__init__()
+
+    def handle_starttag(self, tag, attrs):
+        if tag == 'a':
+            for(attribute, value) in attrs:
+                if attribute == 'href':
+                    
+
+
+
+
+
+    def error(self, message):
+        pass
+
+# finder=LinkFinder()
+# finder.feed('<html><head><title>Test</title></head>'
+#             '<body><h1>Parse me!</h1></body></html>')
