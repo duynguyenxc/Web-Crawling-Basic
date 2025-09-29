2018ea47c821b4347bac7ece85ac820e4631292d
Nguyen Duc Duy <duydeptrai1611@gmail.com>
2025-09-28 23:38:07 -0500

    Add files via upload

---
+++ spider.py
@@ -0,0 +1,30 @@
+from urllib.request import urlopen
+from link_finder import LinkFinder
+from general import*
+
+class Spider:
+
+    #class variable (share among all instances)
+    project_name=''
+    base_url=''
+    domain_name=''
+    queue_file=''
+    crawled_file=''
+    queue=set()
+    crawled=set()
+
+
+    def __init__(self):
+        self
+
+
+
+
+
+
+
+
+
+
+
+
