d553f54e4d0c48c01e373fb698493df3954a4a2c
Nguyen Duc Duy <duydeptrai1611@gmail.com>
2025-10-03 23:16:33 -0500

    add code in domain.py

--- domain.py
+++ domain.py
@@ -1,1 +1,23 @@
-# Create domain.py
+from urllib.parse import urlparse
+
+# Get domain name (example.com)
+def get_domain_name(url):
+    try:
+        results=get_sub_domain_name(url).split('.')
+        return results[-2]+'.'+results[-1]
+    except:
+        return ''
+
+
+
+# Get sub domain name (name.example.com)
+def get_sub_domain_name(url):
+    try:
+        return urlparse(url).netloc
+    except:
+        return ''
+
+
+#print(get_domain_name('http://www.google.com'))
+# print(get_domain_name('https://mail.google.com/mail/u/0/#inbox'))
+# print(get_domain_name('https://dichvucong.gov.vn/p/home/dvc-trang-chu.html'))
