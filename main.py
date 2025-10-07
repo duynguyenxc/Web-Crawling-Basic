29979adb4bb436aaa766161d25af1a0b2f2d33ef
Nguyen Duc Duy <duydeptrai1611@gmail.com>
2025-10-06 23:58:29 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-# main.py
+import threading
+from queue import Queue
+from spider import Spider
+from domain import *
+from general import *
+
+PROJECT_NAME = 'books.toscrape'
+HOMEPAGE = 'http://books.toscrape.com'
+DOMAIN_NAME = get_sub_domain_name(HOMEPAGE)
