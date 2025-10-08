0150eaf97da326c795635059df99fd0b201372c1
Nguyen Duc Duy <duydeptrai1611@gmail.com>
2025-10-07 21:45:34 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -5,5 +5,41 @@ from domain import *
 from general import *
 
 PROJECT_NAME = 'books.toscrape'
-HOMEPAGE = 'http://books.toscrape.com'
+HOMEPAGE = 'https://books.toscrape.com'
 DOMAIN_NAME = get_domain_name(HOMEPAGE)
+QUEUE_FILE = PROJECT_NAME + '/queue.txt'
+CRAWLED_FILE = PROJECT_NAME + '/crawled.txt'
+NUMBER_OF_THREADS = 8
+queue = Queue()
+Spider(PROJECT_NAME, HOMEPAGE, DOMAIN_NAME)
+
+# Create worker threads (will die when main exits)
+def create_workers():
+    for _ in range(NUMBER_OF_THREADS):
+        t=threading.Thread(target=work)
+        t.daemon=True
+        t.start()
+
+# Do the next job in the queue
+def work():
+    while True:
+        url=queue.get()
+        Spider.crawl_page(threading.current_thread().name, url)
+        queue.task_done()
+
+# Each queue link is a new job
+def create_jobs():
+    for link in file_to_set(QUEUE_FILE):
+        queue.put(link)
+    queue.join()
+    crawl()
+
+# Check if there are items in the queue, if so crawl them
+def crawl():
+    queue_links = file_to_set(QUEUE_FILE)
+    if len(queue_links) > 0:
+        print(str(len(queue_links)) + ' links in the queue')
+        create_jobs()
+
+create_workers()
+crawl()
